7c65d0184e132cc8b5936a9faa2da578fcbf5d86
TheFroman <Vertonghengod@gmail.com>
2023-11-12 01:28:13 +0100

    Dodano połowe zagadk

---
+++ zagadka.js
@@ -0,0 +1,5 @@
+var smileyFace = document.getElementById('hehe');
+smileyFace.addEventListener('click', function() {
+    // Display an alert when clicked
+    window.location.href = "https://romangumeniuk.github.io/znalezionanotatka.html";
+  });
