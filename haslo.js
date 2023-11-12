95ef3883f2caed0c23276aee0d91fdd5b582fada
TheFroman <Vertonghengod@gmail.com>
2023-11-12 13:36:47 +0100

    o o

--- haslo.js
+++ haslo.js
@@ -1,8 +1,10 @@
 function myFunction() {
     let text;
+    let text2;
     let person = prompt("Wprowadź hasło:");
     if (person == "jajko") {
       text = "Udało ci sie! Podejdź do mnie po nagrodę na mojej 18";
+      text2 = ""
     } else {
       text = "Niepoprawne hasło";
     }
